e99dd5a884f8eac8bb9e210084ad4aec10c6415d
Diex <diex@roma.local>
2017-11-07 16:02:54 -0300

    la secuencia de carga funciona ok

--- utopias/sound_01/sound_01.ino
+++ utopias/sound_01/sound_01.ino
@@ -10,23 +10,65 @@
 int frameRate = 20;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+#define GO            1
+#define DOWNLOADING   2
+#define WAITING       4
+
+int action = GO;
+
+unsigned long nextUpdate = 3E3;
+unsigned long timeout = 3E3;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  digitalWrite(BUZZER, HIGH); // pongo el pin en alto porque el buzzer esta conectado a 5V.
   pixels.begin(); // This initializes the NeoPixel library.
   Serial.begin(115200);
 }
 
-unsigned long pFrameTime = 0;        // 
+unsigned long pFrameTime = 0;        //
+
 void loop() {
   unsigned long currentTime = millis();
-  if((currentTime - pFrameTime) >= frameRate){
-    pFrameTime = currentTime;
-    playKit();  
-  }
-
-  playSound();
   
+//  if (currentTime >= nextUpdate) {
+//    nextUpdate = currentTime + timeout; // set up the next timeout period
+//    action = action == GO ? DOWNLOADING : GO;
+//  }
+  
+  
+  if ((currentTime - pFrameTime) >= frameRate) {
+    pFrameTime = currentTime;
+    switch (action) {
+      case GO:
+        {
+          if (currentTime >= nextUpdate) {
+             nextUpdate = currentTime + timeout; // set up the next timeout period
+             action = action == GO ? DOWNLOADING : GO;
+          }
+          
+          playKit();
+          silence();
+          break;
+        }
+
+        case DOWNLOADING:
+        {
+          playSound();
+          playDownloading(currentTime);
+
+          break;
+        }
+
+        case WAITING:
+        {
+
+          break;
+        }
+    }
+  }
 }
 
 
@@ -37,8 +79,6 @@ int delayVal = 120;
 int dir = 1;
 
 void playKit() {
-
-    
   if ((millis() - pLedUpdate) >= delayVal) {
     pLedUpdate = millis();
     currentLed = (currentLed + dir);
@@ -49,7 +89,7 @@ void playKit() {
     if (i == currentLed) {
       ledsbuffer[i] = 120;
     } else {
-      Serial.println(ledsbuffer[i]);
+//      Serial.println(ledsbuffer[i]);
       ledsbuffer[i] = constrain(ledsbuffer[i] - 10, 0, 255);;
 
     }
@@ -62,7 +102,65 @@ void playKit() {
 
 }
 
+boolean ledsReseted = false;
+unsigned long downloadingSpeed = 2E3;
 
+void playDownloading(unsigned long currentTime) {
+  if(!ledsReseted){
+    ledsReseted = showEmpty();
+    Serial.println("reseting leds");
+  }else{
+//    Serial.println("leds Reseted");
+    for (int i = 0; i < NUMPIXELS; i++) {
+      if (i == currentLed) {  
+        pixels.setPixelColor(i, pixels.Color(0, 255, 0)); // Moderately bright green color.
+      }      
+    }
+    
+    if ((currentTime - pLedUpdate) >= downloadingSpeed) {
+      pLedUpdate = currentTime;
+      currentLed ++;            
+      if(currentLed > NUMPIXELS){        
+        action = WAITING;
+        ledsReseted = false;
+        reset();
+        silence();
+        nextUpdate = currentTime + 5E3;
+      }
+    }
+    
+    pixels.show(); // This sends the updated pixel color to the hardware.
+  }
+}
+
+
+void reset(){
+  currentLed = 0;
+  dir = 1;
+  for (int i = 0; i < NUMPIXELS; i++) {
+    ledsbuffer[i] = 0;
+    pixels.setPixelColor(i, pixels.Color(ledsbuffer[i], ledsbuffer[i],ledsbuffer[i])); // Moderately bright green color.
+  }
+  pixels.show();
+}
+boolean showEmpty()
+{
+  currentLed = 0;
+  dir = 1;
+  
+  for (int i = 0; i < NUMPIXELS; i++) {
+    ledsbuffer[i] = 0;
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0)); // Moderately bright green color.
+  }
+  pixels.show();
+  return true;
+}
+
+
+
+
+
+// SONIDO
 
 unsigned long previousOn = 0;        // will store last time LED was updated
 unsigned long onInterval = 1000;
@@ -84,6 +182,12 @@ void playSound() {
     tone(BUZZER, random(1000, 4000));
   }
 
+}
+
+void silence()
+{
+  noTone(BUZZER);
+  digitalWrite(BUZZER, HIGH); // pongo el pin en alto porque el buzzer esta conectado a 5V.
 }
 
 
