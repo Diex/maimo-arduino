93053802d156c30d7fbf4b4358e782b14a2b1fee
Diex <diex@roma.local>
2017-11-16 16:12:35 -0300

    agrego sensor al soft de control de las luces

--- vertigo/simple_01/simple_01.ino
+++ vertigo/simple_01/simple_01.ino
@@ -3,7 +3,7 @@
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
@@ -21,6 +21,16 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ80
 int delayval = 500; // delay for half a second
 byte leds[NUMPIXELS];
 
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+#define LEDPin 13 // Onboard LED
+
+int maximumRange = 200; // Maximum range needed 175 es sin nadie
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+
+
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
 #if defined (__AVR_ATtiny85__)
@@ -29,23 +39,97 @@ void setup() {
   // End of trinket special code
 
   pixels.begin(); // This initializes the NeoPixel library.
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+
+  Serial.begin(115200);
+
 }
 
 int offset = 0;
 int current = 0;
 float speed = 0.01;
-#define MULT 4096
-void loop() {
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  current = map(sin(millis()*speed) * MULT, -MULT, MULT, 0, NUMPIXELS);
-  offset++;
-  current = (current+offset) % NUMPIXELS;
-  
-  for(int i=0;i<NUMPIXELS;i++){
-    int value = (i == current) ? 255 : leds[i] - 5;
-    leds[i] = constrain(value, 0, 255);    
-    pixels.setPixelColor(i, pixels.Color(0,leds[i],0)); // Moderately bright green color.
+#define MULT 4096
+int decay = 5;
+
+#define RUNNING 1
+#define IDLE 2
+
+int state = RUNNING;
+int limit = 175;
+boolean isPeople = false;
+
+void loop() {
+  sensor();
+
+  if (distance < limit) {
+    state = RUNNING;
+  } else {
+    state = IDLE;
   }
-  pixels.show(); // This sends the updated pixel color to the hardware.
+
+  switch (state) {
+
+    case RUNNING:
+      // speed control
+      speed = sin(millis() * 0.005) * 0.0005;
+//      Serial.println(speed);
+      // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+      current = map(sin(millis() * speed) * MULT, -MULT, MULT, 0, NUMPIXELS);
+      offset++;
+      current = (current + offset) % NUMPIXELS;
+
+      for (int i = 0; i < NUMPIXELS; i++) {
+        int value = (i == current) ? 255 : leds[i] - decay;
+        leds[i] = constrain(value, 0, 255);
+        pixels.setPixelColor(i, pixels.Color(leds[i], leds[i], leds[i])); // Moderately bright green color.
+      }
+      pixels.show(); // This sends the updated pixel color to the hardware.
+
+
+      break;
+
+    case IDLE:
+      for (int i = 0; i < NUMPIXELS; i++) {
+        int value = leds[i] - decay;
+        leds[i] = constrain(value, 0, 255);
+        pixels.setPixelColor(i, pixels.Color(leds[i], leds[i], leds[i])); // Moderately bright green color.
+      }
+      pixels.show(); // This sends the updated pixel color to the hardware.
+
+
+      break;
+
+  }
+
+
 }
+
+void sensor() {
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  //Calculate the distance (in cm) based on the speed of sound.
+  distance = duration / 58.2;
+  if (distance >= maximumRange || distance <= minimumRange) {
+    /* Send a negative number to computer and Turn LED ON
+      to indicate "out of range" */
+    Serial.println("-1");
+    digitalWrite(LEDPin, HIGH);
+  }
+  else {
+    /* Send the distance to the computer using Serial protocol, and
+      turn LED OFF to indicate successful reading. */
+    Serial.println(distance);
+    digitalWrite(LEDPin, LOW);
+  }
+}
+
+
