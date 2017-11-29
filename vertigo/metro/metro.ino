80ea08e545b08e3c0978d4dac82f7482293c3cb1
Diex <diex@roma.local>
2017-11-28 23:39:09 -0300

    mi metronomo funciona en BPMs

--- vertigo/metro/metro.ino
+++ vertigo/metro/metro.ino
@@ -7,7 +7,7 @@
 #endif
 
 #define PIN 6
-#define NUMPIXELS 58
+#define NUMPIXELS 56
 #define MAX_POSITION NUMPIXELS-1
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -25,22 +25,33 @@ void setup()
 int position = 0;
 int decay = 10;
 
-int frameDuration = 5;
+int frameDuration = 2;
 unsigned long frameCount = 0;
 byte speed = 255;
+
+unsigned long minPeriod = frameDuration * NUMPIXELS;
+unsigned long lastStep = 0;
 void loop()
 {
 			// enterFrame();
 			unsigned long frameStart = millis();						
 			// beginDraw();
-			position = (frameCount * speed >> 8) % NUMPIXELS;
-			for (int i = 0; i < NUMPIXELS; i++)
-			{
-				leds[i] = (i == position)? 255: 0; //leds[i] - decay;
-				leds[i] = constrain(leds[i], 0, 255);
-				pixels.setPixelColor(i, pixels.Color(leds[i], leds[i], leds[i])); 
+			// position = (frameCount * speed >> 8) % NUMPIXELS;
+
+			// 60,000 ms (1 minute) / Tempo (BPM) = Delay Time in ms for quarter-note beats
+			if(frameStart - lastStep > 60E3 / 60 / NUMPIXELS){ 
+				lastStep = frameStart;
+				position = pingPong(position, MAX_POSITION);
+				for (int i = 0; i < NUMPIXELS; i++)
+				{
+					leds[i] = (i == position)? 255: 0; //leds[i] - decay;
+					leds[i] = constrain(leds[i], 0, 255);
+					pixels.setPixelColor(i, pixels.Color(leds[i], leds[i], leds[i])); 
+				}
+				
+				pixels.show();	
 			}
-			pixels.show();
+			
 
 			// exitFrame();
 			// basicamente desperdicio el tiempo que me sobro.
@@ -49,5 +60,19 @@ void loop()
 			frameCount ++;
 			digitalWrite(13, LOW);
 			
+}
+
+boolean up = true; 
+int pingPong(int position, int maxPos)
+{
+	int newpos = 0;
+	if(up){
+		up = position+1 > maxPos ? false : true;  // el ultimo encendido...
+		newpos = position+1 > maxPos ? maxPos : position+1;
+	}else{
+		up = position-1 < 0 ? true : false; // el primero
+		newpos = position-1 < 0 ? 0 : position-1;
+	}
+	return newpos; 
 }
 
