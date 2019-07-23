e8a03468abbc720834238eee26eb06684d3cb0b3
Diex <diex@roma.local>
2019-07-22 23:28:47 -0300

    puedo recibir mensajes GET y parsearlos

--- utopias/2019/comm_02/comm_02.ino
+++ utopias/2019/comm_02/comm_02.ino
@@ -2,65 +2,81 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+// server de control del robot infante
 
-const char* ssid     = "suckmykiss"; // the ssid/name of the wifi, the esp will be connected to
-const char* password = "frutigran"; // the password of that wifi
+const char* ssid     = "suckmykiss";  // the ssid/name of the wifi, the esp will be connected to
+const char* password = "frutigran";   // the password of that wifi
 
-const char* assid = "espAccessPoint";
-const char* asecret = "hello";
+const char* assid = "infante";
+const char* asecret = "umai";
 
 ESP8266WebServer server(80);
-int led = 2;
 
-
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
 void handleRoot() {
-  server.send(200, "text/html", "<h1>You are connected</h1>");
-
+  server.send(200, "text/html", "<h1>Usted se ha conectado a Robot Infante</h1>");
 }
 
-void handleNotFound(){
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+void handleNotFound() {
+  server.send(404, "text/plain", "404: :( "); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
 
-void hikido(){  
-  digitalWrite(led,!digitalRead(led));      // Change the state of the LED  
-  server.sendHeader("Location","/button");        // Add a header to respond with a new location for the browser to go to the home page again
-  server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
-}
-
-void handleButton(){  
+void handleButton() {
   server.send(200, "text/html", "<form action=\"/hikido\" method=\"POST\"><input type=\"submit\" value=\"Toggle LED\"></form>");
 }
 
+
 char *message;
 char data[256];
+int messageId = -1;
+
+#define DATA_IN   2
+#define SHOW_DATA 1
+#define ROBOT_GO  0
+
+
 char datain[] = "datain";
+char showdata[] = "showdata";
+char robotgo[] = "robotgo";
 
-void handleMessage(){
-  Serial.println(server.uri());
-  Serial.println(server.method());
-  Serial.println(server.args());
+int dataValue = 0;
 
-  for (uint8_t i = 0; i < server.args(); i++) {
-    
-    Serial.println( "" + server.argName(i) + ": " + server.arg(i));
-    
-    char temp[256];
-    server.argName(i).toCharArray(temp,256);
-    
-    Serial.println(temp);
+boolean newdata = false;
+boolean newmessage = false;
 
-    if(strncmp(temp, datain, strlen(datain)) == 0){
-      Serial.println("======");
-      server.arg(i).toCharArray(data, 256);
-      Serial.println(data);
-    } 
+#define LED 2
+
+void handleMessage() {
+  
+  newmessage = true;
+  
+  char arg[256];
+  char val[256];
+
+  for (uint8_t i = 0; i < server.args(); i++) {    
+    
+    Serial.print(server.argName(i));
+    Serial.print(": ");
+    Serial.println(server.arg(i));
+    
+
+    server.argName(i).toCharArray(arg, 256);
+    server.arg(i).toCharArray(val, 256);
+    
+    if (strncmp(arg, datain, strlen(datain)) == 0) {
+      messageId = DATA_IN;
+      
+      if (!newdata) { 
+        server.arg(i).toCharArray(data, 256);
+        newdata = true;     
+      }
+
+    }else if(strncmp(arg, robotgo, strlen(robotgo)) == 0){
+      messageId = ROBOT_GO;
+      dataValue = (strncmp(val, "0", 1) == 0) ? 0 : 1; 
+    }
   }
 
-  
+
 
   server.send(200, "text/plain", "message received");
   // server.sendHeader("Location","/message");        // Add a header to respond with a new location for the browser to go to the home page again
@@ -69,16 +85,17 @@ void handleMessage(){
 
 
 
-void setup(){
+void setup() {
+
   Serial.begin(115200);
-  pinMode(led, OUTPUT);
-  
+  pinMode(LED, OUTPUT);
+
   WiFi.mode(WIFI_AP_STA);
- 
 
   //access point part
   Serial.println("Creating Accesspoint");
-  WiFi.softAP(assid,asecret,7,0,5);
+  WiFi.softAP(assid, asecret, 7, 0, 5);
+
   Serial.print("IP address:\t");
   Serial.println(WiFi.softAPIP());
 
@@ -86,9 +103,9 @@ void setup(){
   Serial.print("connecting to...");
   Serial.println(ssid);
 
-  WiFi.begin(ssid,password);
+  WiFi.begin(ssid, password);
 
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -96,7 +113,7 @@ void setup(){
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());   
+  Serial.println(WiFi.localIP());
 
 
   //   we send our IP address on the WiFi network
@@ -108,24 +125,47 @@ void setup(){
   }
 
   Serial.println("mDNS responder started");
-  // Add service to MDNS-SD
-  // MDNS.addService("http", "tcp", 80);
 
   server.on("/", handleRoot);
   server.on("/button", handleButton);
-  server.on("/hikido", HTTP_POST, hikido);  // Call the 'handleLED' function when a POST request is made to URI "/LED"
   server.on("/message", handleMessage);
 
-  server.onNotFound(handleNotFound); 
+  server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("HTTP server started");
 
-  
+
 }
 
 void loop() {
   MDNS.update();
   server.handleClient();
-  // tone(3, random(1000), 100);
+ 
+  processMessage(); // tone(3, random(1000), 100);
+
+}
+
+void processMessage(){
+  if(!newmessage) return;
   
+  Serial.println("new message");
+
+  newmessage = false;
+
+  switch(messageId){
+    case ROBOT_GO:
+      digitalWrite(LED, dataValue);
+    break;
+
+    case SHOW_DATA:
+      // blabla...
+    break;
+
+    case DATA_IN:
+
+    break;
+
+
+  }
+
 }
